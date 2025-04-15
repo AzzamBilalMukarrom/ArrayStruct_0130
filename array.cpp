c1acd4400df6d29e3e11302960ca3caee97d13ce
AzzamBilalMukarrom <bilaldenker020@gmail.com>
2025-04-15 20:33:30 +0700

    array cpp clear

--- array.cpp
+++ array.cpp
@@ -8,17 +8,25 @@ int skor[4]={12, 24, 48, 64};
 //deklarasi array dengan ukuran, tanpa nilai awal
 string nama[3];
 
+   
 int main() {
+
+
     // menampilkan isi array pada index tertentu
     cout << "Tampilan skor ke 3 = " << skor [2] << endl;
+
+
     // mengisi array dengan loop for
+
     for(int n = 0; n < 3; n++){
         cout << "Mahasiswa ke- " << n+1 << endl;
         cout << "Nama Mahasiswa = " ;
         cin >> nama[n];
     }
     cout << endl;
-    // menampilkan isi array
+    // menampilkan isi array di terminal
+
+
     for(int n = 0; n < 3; n++){
         cout << "data ke- " << n+1 << endl;
         cout << "data Mahasiswa = " << nama[n] << endl;
