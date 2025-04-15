39b46230e80e8959da9735b22ce7f2ecb1e9ecdb
AzzamBilalMukarrom <bilaldenker020@gmail.com>
2025-04-15 20:11:28 +0700

    Mengisi Array Dengan Loop For

---
+++ struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+ struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+
+ };
+
+ int main (){
+    //Membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    //mengakses struct mahasiswa
+    mhs.nim = "2022014000";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat";
+    cin >> mhs.alamat;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "NIM = " << mhs.nama << endl;
+    cout << "NIM = " << mhs.alamat << endl;
+ }
